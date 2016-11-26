75f4b7a939840c67148de8b71ea82942ee3c2048
Max Milton <max@wearegenki.com>
2016-11-27 10:11:57 +1100

    Debugging permissions issue 15

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,8 @@ fi
 
 # Make exim become pid 1 and switch to exim user
 if [ "$1" = 'exim' ] && [ "$(id -u)" = '0' ]; then
-  exec su-exec exim "$0" "$@"
+	# chown -R exim: /var/log/exim /usr/lib/exim /var/spool/exim
+  # exec su-exec exim "$0" "$@"
 fi
 
 exec "$@"
